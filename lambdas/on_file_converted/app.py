f7fee763778bcd4ccbc95b610c9e82e06cae2773
Vinicius Soares Batista <vbatista@gmail.com>
2022-01-11 20:42:58 -0300

    fixed logic after file is converted

--- lambdas/on_file_converted/app.py
+++ lambdas/on_file_converted/app.py
@@ -1,17 +1,56 @@
+from typing import Optional
 from aws_lambda_powertools.logging.logger import Logger
 import json
 import boto3
+import json
+import datetime
 
 logger = Logger()
-eventBridge = boto3.client("events")
 
 def handler(event, context):
-    fileName = event["detail"]["filename"]
+    filename = event["detail"]["filename"]
     fileContent = event["detail"]
 
     logger.info({
-        "message": "Received event for file {}".format(fileName),
+        "message": "Received event for file {}".format(filename),
         "fileContent": fileContent
     })
 
-    #TODO: validate on lambda(port simulator to here)
+    validationMessage = validateContent(filename, fileContent)
+    generateEvent(json.dumps({"filename": filename, "validationMessage": validationMessage}))  
+    
+def validateContent(filename, payload):
+    """
+        Validates the json payload
+    """
+    logger.debug({
+        "message": "Performing file validation for file{}".format(filename)
+    })
+
+    if len(payload["nf"]["infLeitura"]["placa"]) != 7:
+        return "PLACA_INVALIDA"
+    return None
+
+
+def generateEvent(payload, status="file-validated"):
+    eventBridge = boto3.client("events")
+    logger.info({
+        "message": "Sending event {}".format(status),
+        "payload": payload
+    })
+    event = {
+        "Time": datetime.datetime.now(),
+        "Source": "NFProcessor.file_validator",
+        "DetailType": status,
+        "Detail": payload
+    }
+    logger.debug({
+        "message": "event final payload",
+        "payload": payload
+        })
+
+    eventBridgePutReturn = eventBridge.put_events(Entries = [event])
+    logger.debug({
+        "message": "Event Bridge PUT return",
+        "eventBridgePutReturn": eventBridgePutReturn
+        })
