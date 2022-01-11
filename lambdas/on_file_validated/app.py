4609c9bb93cba57dab443e80feb0c9d07c7ac689
Vinicius Soares Batista <vbatista@gmail.com>
2022-01-11 20:42:13 -0300

    added on file validated event for final processing/add to dynamodb

---
+++ lambdas/on_file_validated/app.py
@@ -0,0 +1,43 @@
+from aws_lambda_powertools.logging.logger import Logger
+import boto3
+import os
+
+logger = Logger()
+ddb = boto3.resource('dynamodb')
+ddb_resource = boto3.resource('dynamodb')
+_ddb_table_name = os.getenv("DDB_TABLE_NAME")
+
+
+def handler(event, context):
+    filename = event["detail"]["filename"]
+    validationMessage = event["detail"]["validationMessage"]
+
+    saveToDatabase(filename, validationMessage)
+
+    logger.info({
+        "message": "Received request to save event for file {} on database".format(filename)
+    })
+
+
+def saveToDatabase(filename, validationMessage):
+    """
+    Saves the record to the database
+    """
+    table = ddb_resource.Table(_ddb_table_name)
+    status = "OK" if validationMessage is None else "NOT_OK"
+    logger.info({
+        "message": "Saving record on ddb for file {} with status {}".format(filename, status),
+        "validationMessage": validationMessage
+    })
+    response= table.put_item(
+        Item={
+            'filename': filename,
+            'status': status,
+            'validation_error': validationMessage
+            }
+    )
+
+    logger.debug({
+        "message": "Return from DDB",
+        "ddb_return": response
+    })
